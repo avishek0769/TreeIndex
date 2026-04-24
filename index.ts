a0e438393c4d60fa490803baee14363df0078917
avishek0769 <avishekadhikary42@gmail.com>
2026-04-24 16:20:50 +0530

    Setup the project, with a story data

---
+++ index.ts
@@ -0,0 +1,17 @@
+import OpenAI from "openai";
+import { sampleData } from "./sample_data"
+
+const openai = new OpenAI({
+    baseURL: "https://openrouter.ai/api/v1",
+    apiKey: process.env.OPENROUTER_KEY
+})
+
+const SYSTEM_PROMPT = `
+
+`
+
+async function generateKnowledgeTree () {
+    
+}
+
+generateKnowledgeTree()
